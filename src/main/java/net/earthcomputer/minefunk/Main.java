d21ff4cd4f0c085ac8e1f4151df308b4c5467e12
Earthcomputer <burtonjae@hotmail.co.uk>
2017-05-30 07:46:25 +0100

    Forgot an import

--- src/main/java/net/earthcomputer/minefunk/Main.java
+++ src/main/java/net/earthcomputer/minefunk/Main.java
@@ -9,6 +9,7 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Calendar;
